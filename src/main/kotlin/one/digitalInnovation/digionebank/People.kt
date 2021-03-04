686910716a29b167818faeed0d57d6f0852e65e6
ray-sales <portifoliorayssa@gmail.com>
2021-03-04 19:26:11 -0300

    Update to apply modifier private set

--- src/main/kotlin/one/digitalInnovation/digionebank/People.kt
+++ src/main/kotlin/one/digitalInnovation/digionebank/People.kt
@@ -3,11 +3,11 @@ package one.digitalInnovation.digionebank
 class People {
     var name: String = "Ray"
     var cpf: String = "123.456.789-90"
+    private set
 }
 
 fun main(){
     val rayssa = People()
-
     println(rayssa.name)
     println(rayssa.cpf)
 
