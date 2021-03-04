72b53ea8610d2aa7f05cddc772619cb9b274be9c
ray-sales <portifoliorayssa@gmail.com>
2021-03-04 19:56:42 -0300

    Created account class

---
+++ src/main/kotlin/one/digitalInnovation/digionebank/Account.kt
@@ -0,0 +1,17 @@
+package one.digitalInnovation.digionebank
+
+import java.math.BigDecimal
+
+class Account(
+    val agency: String,
+    val numberAccount: String,
+    val amount: BigDecimal
+) {
+    fun deposit(value:BigDecimal){
+
+    }
+
+    fun plunder(value:BigDecimal){
+        
+    }
+}
