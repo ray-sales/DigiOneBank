0fa3fb5b495d8e7fa5dd28621277b949bcc93b89
ray-sales <portifoliorayssa@gmail.com>
2021-03-04 19:06:36 -0300

    Criação do projeto Kotlin

---
+++ src/main/kotlin/one/digitalInnovation/digionebank/People.kt
@@ -0,0 +1,14 @@
+package one.digitalInnovation.digionebank
+
+class People {
+    var name: String = "Ray"
+    var cpf: String = "123.456.789-90"
+}
+
+fun main(){
+    val rayssa = People()
+
+    println(rayssa.name)
+    println(rayssa.cpf)
+
+}
